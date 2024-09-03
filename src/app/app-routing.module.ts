e3c67a0e9cfcad1426f269d931c432f3096243b9
JUAN CARLOS ESTEVEZ HIDALGO <juan.estevez@iess.gob.ec>
2024-09-03 15:41:26 -0500

    Add Sakai Theme

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppLayoutComponent } from './layout/app.layout.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: AppLayoutComponent,
+    children: [
+      {
+        path: '', component: HomeComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
