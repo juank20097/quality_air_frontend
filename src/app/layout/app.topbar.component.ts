8b2155b892bb3e8d489000762bb639f32b84df1d
JUAN CARLOS ESTEVEZ HIDALGO <juan.estevez@iess.gob.ec>
2024-09-05 09:46:56 -0500

    add logout with Dialog

--- src/app/layout/app.topbar.component.ts
+++ src/app/layout/app.topbar.component.ts
@@ -1,10 +1,13 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { MenuItem } from 'primeng/api';
 import { LayoutService } from "./service/app.layout.service";
+import { ConfirmationService } from 'primeng/api';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-topbar',
-    templateUrl: './app.topbar.component.html'
+    templateUrl: './app.topbar.component.html',
+    providers: [ConfirmationService]
 })
 export class AppTopBarComponent {
 
@@ -16,6 +19,28 @@ export class AppTopBarComponent {
 
     @ViewChild('topbarmenu') menu!: ElementRef;
 
-    constructor(public layoutService: LayoutService) { }
+    constructor(public layoutService: LayoutService,private confirmationService: ConfirmationService, private router: Router) { }
+
+    confirm1() {
+      this.confirmationService.confirm({
+          key: 'confirm1',
+          message: 'Está seguro de Cerrar Sesión?',
+          accept: () => {
+            this.router.navigate(['/login']);
+          },
+          reject: () => {
+            console.log('Logout cancelled');
+          }
+      });
+  }
+
+    confirmLogout() {
+        this.confirmationService.confirm({
+          message: 'Are you sure you want to log out?',
+          header: 'Logout Confirmation',
+          icon: 'pi pi-exclamation-triangle',
+          
+        });
+      }
 
 }
