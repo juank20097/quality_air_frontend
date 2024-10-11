e3be8ba042f3479026900df74174a18258e21dfa
JUAN CARLOS ESTEVEZ HIDALGO <juan.estevez@iess.gob.ec>
2024-10-11 16:41:45 -0500

    change name of project

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     keycloakUrl: 'http://localhost:8081',
-    keycloakReaml: 'devops',
+    keycloakReaml: 'Air-Quality',
     keycloakClient: 'base',
     urlLogout:'http://localhost'
   };
