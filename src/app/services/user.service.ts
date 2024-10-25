0b51612cd7650cfe9256e10a4d935d1111064db7
Ivan9898989 <ivan.simbana@epn.edu.ec>
2024-10-25 13:27:14 -0500

    Actualizacion 2

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -14,5 +14,14 @@ export class UserService {
   getUsers(): Observable<User[]> {
     return this.http.get<User[]>(this.apiUrl);
   }
+
+  addUser(user: User): Observable<User> {
+    return this.http.post<User>(this.apiUrl, user);
+  }
+
+  updateUser(user: User): Observable<User> {
+    return this.http.put<User>(`${this.apiUrl}/${user.id}`, user);
+  }
   
+
 }
